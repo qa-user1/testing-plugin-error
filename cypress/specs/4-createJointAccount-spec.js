596c24f0d4e82f2034477a5a6509acf0813195a3
AmminaSK <senderovic.amina@gmail.com>
2023-02-21 12:11:58 +0100

    stability test 4

--- cypress/specs/4-createJointAccount-spec.js
+++ cypress/specs/4-createJointAccount-spec.js
@@ -30,6 +30,7 @@ context('Log in to the Nucleus Wealth portal, create a Joint Account and complet
             .verify_non_super_subtypes()
             .select_joint_non_super_subtype()
             .click_create_investment_account()
+            .go_through_tour_steps(C.stepMessages)
             .verify_investment_choice_page()
         cy.saveLocalStorage();
 
