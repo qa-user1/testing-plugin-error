53a9bb22d03139149a87f1177761c52b8dc1a4a9
AmminaSK <senderovic.amina@gmail.com>
2023-02-22 19:52:56 +0100

    Update 8-signUpNewUser-spec.js

--- cypress/specs/8-signUpNewUser-spec.js
+++ cypress/specs/8-signUpNewUser-spec.js
@@ -17,7 +17,7 @@ context('Sign Up new user', () => {
 
 
     it('1. Create a new user', function () {
-        if (Cypress.env('cypressRunnerLocal') === false) {
+        if (Cypress.env('cypressRunnerLocal') === true) {
             ui.app.clear_gmail_inbox()
         }
             ui.login.open_base_url()
