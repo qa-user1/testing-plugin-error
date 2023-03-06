a951601a4af036948cd17e4212da8699429e04b2
AmminaSK <senderovic.amina@gmail.com>
2023-03-06 12:15:40 +0100

    ...

---
+++ cypress.config.js
@@ -0,0 +1,35 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  screenshotsFolder: './cypress/snapshots',
+  trashAssetsBeforeRuns: true,
+  defaultCommandTimeout: 100000,
+  requestTimeout: 100000,
+  taskTimeout: 150000,
+  responseTimeout: 100000,
+  pageLoadTimeout: 100000,
+  video: false,
+  viewportHeight: 900,
+  viewportWidth: 1000,
+  watchForFileChanges: false,
+  chromeWebSecurity: false,
+  screenshotOnRunFailure: true,
+  env: {
+    allureResultsPath: 'report/allure-results',
+    allure: true,
+    failSilently: false,
+    failOnSnapshotDiff: true,
+    cypressRunnerLocal: true,
+  },
+  retries: 0,
+  projectId: 'fmx7mz',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: './cypress/specs/**/*.{js,jsx,ts,tsx}',
+    excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
+  },
+})
