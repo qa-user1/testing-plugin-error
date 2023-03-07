2dc957e02dcdcb20b117d10bd922729b2a9c1158
AmminaSK <senderovic.amina@gmail.com>
2023-03-07 11:49:09 +0100

    all tests

--- cypress.config.js
+++ cypress.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
     cypressRunnerLocal: true,
   },
   retries: 0,
-  projectId: '4k7igv',
+  projectId: '6ccunw',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
