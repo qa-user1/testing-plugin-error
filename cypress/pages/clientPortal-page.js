03d6f3d888c4a0860659e126c681f817b8a2dab3
AmminaSK <senderovic.amina@gmail.com>
2023-03-08 22:14:15 +0100

    Update clientPortal-page.js

--- cypress/pages/clientPortal-page.js
+++ cypress/pages/clientPortal-page.js
@@ -360,7 +360,7 @@ export default class LoginPage extends BasePage {
          //cy.task('loadData').then(accountNo => {
         cy.session({cache: true}, () => {
             const accountNo = window.sessionStorage.getItem('accountNo');
-             cy.contains(accountNo).parents('.ant-card-body')
+             cy.contains('accountNo').parents('.ant-card-body')
                  .should('exist')
                  .within(() => {
                      cy.contains('Change Portfolio').click()
