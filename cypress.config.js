59cb5957ccfd439f63900ca2014f3a9de1b106ff
AmminaSK <senderovic.amina@gmail.com>
2023-03-09 14:52:53 +0100

    ...

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('cypress')
 const getCompareSnapshotsPlugin = require("cypress-visual-regression/dist/plugin");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+const excel = require("./cypress/support/excel-helper");
+const imaps = require("imap-simple");
 
 module.exports = defineConfig({
   screenshotsFolder: './cypress/snapshots',
@@ -9,7 +12,7 @@ module.exports = defineConfig({
   taskTimeout: 150000,
   responseTimeout: 100000,
   pageLoadTimeout: 100000,
-  video: true,
+  video: false,
   viewportHeight: 900,
   viewportWidth: 1000,
   watchForFileChanges: false,
@@ -38,6 +41,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       const getCompareSnapshotsPlugin = require('cypress-visual-regression/dist/plugin');
       getCompareSnapshotsPlugin(on, config);
+
+      const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+      allureWriter(on, config);
+
+      const imaps = require('imap-simple');
+      const fs = require('fs');
+      var path = require('path');
+      const _ = require('lodash');
+
       const data = {}
       on('task', {
         saveData(x) {
@@ -50,6 +62,80 @@ module.exports = defineConfig({
           console.log('returning accountNo', data.accountNo)
           return data['accountNo'] || null
         },
+
+        generate_excel_file(args) {
+
+          let filename = args.filename;
+          let dataObject = args.dataObject;
+
+          let excel = require('../testing-plugin-error/cypress/support/excel-helper');
+          return new Promise((resolve, reject) => {
+            excel.generate_file(filename, dataObject);
+            return resolve(true)
+          });
+        },
+
+        log(message) {
+          console.log(message);
+          return null
+        },
+
+        fetchGmailUnseenMails({username, password, markSeen}) {
+          var config = {
+            imap: {
+              user: username,
+              password: password,
+              host: 'imap.gmail.com',
+              port: 993,
+              tls: true,
+              authTimeout: 5000
+            }
+          };
+
+          return imaps.connect(config).then(function (connection) {
+
+            return connection.openBox('INBOX').then(function () {
+              var searchCriteria = [
+                'UNSEEN'
+              ];
+
+              var fetchOptions = {
+                bodies: ['HEADER', 'TEXT'],
+                markSeen: markSeen
+              };
+
+              return connection.search(searchCriteria, fetchOptions).then(function (results) {
+                var mails = [];
+                if (results) {
+                  results.forEach(item => {
+                    var _mail = {};
+                    if (item && item.parts) {
+                      item.parts.forEach(_item => {
+                        if (_item.which == "TEXT") {
+                          _mail.body = _item.body;
+                        }
+                        if (_item.which == "HEADER" && _item.body && _item.body.from && _item.body.from.length) {
+                          _mail.from = _item.body.from[0];
+                        }
+                        if (_item.which == "HEADER" && _item.body && _item.body.subject && _item.body.subject.length) {
+                          _mail.subject = _item.body.subject[0];
+                        }
+                        if (_item.which == "HEADER" && _item.body && _item.body.to && _item.body.to.length) {
+                          _mail.to = _item.body.to[0];
+                        }
+                      })
+                    }
+                    mails.push(_mail);
+                  });
+                  //connection.end()
+                }
+                return mails;
+              });
+            });
+          });
+        },
+
+
       })
       return config;
     },
