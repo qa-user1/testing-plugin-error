4c6be21eff57a9c6812dc005a8c571eed369cb13
AmminaSK <senderovic.amina@gmail.com>
2023-03-16 15:34:08 +0100

    ...

--- cypress/specs/1.10-ClientPortalAccountDashboard.js
+++ cypress/specs/1.10-ClientPortalAccountDashboard.js
@@ -3,188 +3,114 @@ const D = require('../fixtures/data');
 const C = require('../fixtures/constants');
 
 context('Client Portal - Account Dashboard', () => {
-
+    let accountNo;
 
     beforeEach(function () {
-        cy.clearAllLocalStorage()
-        cy.clearAllCookies()
-        cy.clearAllSessionStorage()
+        /*   cy.clearAllLocalStorage()
+           cy.clearAllCookies()
+           cy.clearAllSessionStorage()*/
+
     })
 
 
+    it('1. Validate login credentials', function () {
+        ui.login.open_base_url()
+            .verify_login_menu(D.user)
+            .enter_credentials_and_click_Sign_In(D.user.username, D.user.password)
+            .redirect_user_to_the_create_a_new_account_page()
+        cy.visit('https://testwebserver.nucleuswealth.com/onboarding/5538/success')
+        // ui.onboarding.store_current_account_number(accountNo)
+        cy.get('[data-test="onboarding-rightHeader-title"]').invoke('text').then(function (text) {
+            cy.log('ACCOUNT NUMBER ' + text)
+            accountNo = text.match('Account (' + "(.*)" + ')')[1];
+
+            cy.saveLocalStorage()
+        })
+    })
+
     it('1. Direct user to “Your Accounts” page', () => {
-        if (Cypress.env('skipError')) {
-            cy.log('Skipping test due to error in config file')
-            return;
-        }
-        try {
-            ui.login.open_base_url()
-                .verify_login_menu(D.user)
-            ui.login.enter_credentials_and_click_Sign_In(D.user.username, D.user.password)
-            ui.clientPortal.click_your_accounts_link()
-                .verify_your_accounts_page()
-            ui.clientPortal.verify_your_accounts_page()
-        } catch (error) {
-            if (error.code === 'ECONNRESET') {
-                cy.log('Skipping test due to ECONNRESET error')
-                return;
-            }
-        }
+
+
+        ui.login.open_base_url()
+            .verify_login_menu(D.user)
+        ui.login.enter_credentials_and_click_Sign_In(D.user.username, D.user.password)
+        ui.clientPortal.click_your_accounts_link()
+            .verify_your_accounts_page()
     })
 
 
     it('2. Overall asset summary panel', function () {
-        if (Cypress.env('skipError')) {
-            cy.log('Skipping test due to error in config file')
-            return;
-        }
-        try {
-            ui.clientPortal.verify_overall_asset_summary_panel('0')
-        } catch (error) {
-            if (error.code === 'ECONNRESET') {
-                cy.log('Skipping test due to ECONNRESET error')
-                return;
-
-            }
-        }
+        ui.clientPortal.verify_overall_asset_summary_panel('0')
 
     })
 
 
     it('3. Direct user to Account Dashboard', function () {
-        if (Cypress.env('skipError')) {
-            cy.log('Skipping test due to error in config file')
-            return;
-        }
-        try {
-            ui.clientPortal.check_investment_account_panel()
-                .verify_target_weight_total()
-                .verify_content_of_investment_account_panel()
-                .click_view_account_details()
-                .verify_account_dashboard()
-        } catch (error) {
-            if (error.message.includes('read ECONNRESET')) {
-                cy.log('Caught "read ECONNRESET" error')
-                // Return early here
-                return;
-            }
-            // No error or a different error was thrown, continue with the test
-            cy.log('No error or different error was thrown')
-            // Add more commands here
-        }
+
+        ui.clientPortal.check_investment_account_panel()
+            .verify_target_weight_total(accountNo)
+            .verify_content_of_investment_account_panel(accountNo)
+            .click_view_account_details(accountNo)
+            .verify_account_dashboard()
     });
 
 
 
     it('4. Check Tactical Panel', function () {
-        if (Cypress.env('skipError')) {
-            cy.log('Skipping test due to error in config file')
-            return;
-        }
-        try {
-            ui.login.open_base_url()
-            ui.clientPortal.click_your_accounts_link()
-                .click_view_account_details()
-            ui.clientPortal.click_tactical_panel()
-                .verify_tactical_headings()
-                .click_additional_assets()
-                .verify_additional_assets_input_fields()
-                .enter_cash_and_own_home_values(D.tacticalAdditionalAssets)
-        } catch (error) {
-            if (error.code === 'read ECONNRESET') {
-                cy.log('Skipping test due to ECONNRESET error')
-                return;
 
-            }
-        }
+        ui.login.open_base_url()
+        ui.clientPortal.click_your_accounts_link()
+            .click_view_account_details(accountNo)
+        ui.clientPortal.click_tactical_panel()
+            .verify_tactical_headings()
+            .click_additional_assets()
+            .verify_additional_assets_input_fields()
+            .enter_cash_and_own_home_values(D.tacticalAdditionalAssets)
     })
 
     it('5. Expand Strategic', function () {
-        if (Cypress.env('skipError')) {
-            cy.log('Skipping test due to error in config file')
-            return;
-        }
-        try {
-            ui.clientPortal.click_strategic_panel()
-                .compare_snapshots()
-                .verify_change_portfolio_button()
-                .click_strategic_panel()
-        } catch (error) {
-            if (error.code === 'ECONNRESET') {
-                cy.log('Skipping test due to ECONNRESET error')
-                return;
 
-            }
-        }
-        /*if (Cypress.env('skipError')) {
-            cy.log('Skipping test due to error in config file')
-            return;
-        }
-                ui.clientPortal.click_strategic_panel()
-                    .compare_snapshots()
-                    .verify_change_portfolio_button()
-                    .click_strategic_panel()*/
+        ui.clientPortal.click_strategic_panel()
+            .compare_snapshots()
+            .verify_change_portfolio_button()
+            .click_strategic_panel()
+
     })
 
 
     it('6. Expand Ethics/Exclusions', function () {
-        if (Cypress.env('skipError')) {
-            cy.log('Skipping test due to error in config file')
-            return;
-        }
-        try {
-            ui.clientPortal.click_ethics_panel()
-            ui.onboarding.verify_chosen_ethics([
-                ['Climate Change', ['No Fossil Fuels (Worst Offenders)', 'No Fossil Fuels (Any)']],
-                ['War', ['No Arms (Any)']]
-            ])
-            ui.clientPortal.verify_change_ethics_button()
-                .click_ethics_panel()
-        } catch (error) {
-            if (error.code === 'ECONNRESET') {
-                cy.log('Skipping test due to ECONNRESET error')
-                return;
 
-            }
-        }
+        ui.clientPortal.click_ethics_panel()
+        ui.onboarding.verify_chosen_ethics([
+            ['Climate Change', ['No Fossil Fuels (Worst Offenders)', 'No Fossil Fuels (Any)']],
+            ['War', ['No Arms (Any)']]
+        ])
+        ui.clientPortal.verify_change_ethics_button()
+            .click_ethics_panel()
+
 
     })
 
     it('7. Expand Portfolio', function () {
-        try {
-            ui.clientPortal.click_portfolio_panel()
-                .verify_change_ethics_button2()
-                .verify_change_portfolio_button2()
-                .verify_nucleus_portfolio_allocations()
-                .verify_security_holdings()
-                .verify_security_column()
-                .click_portfolio_panel()
-        } catch (error) {
-            if (error.code === 'ECONNRESET') {
-                cy.log('Skipping test due to ECONNRESET error')
-                return;
 
-            }
-        }
+        ui.clientPortal.click_portfolio_panel()
+            .verify_change_ethics_button2()
+            .verify_change_portfolio_button2()
+            .verify_nucleus_portfolio_allocations()
+            .verify_security_holdings()
+            .verify_security_column()
+            .click_portfolio_panel()
 
     })
 
     it('8. Expand Performance', function () {
-        try {
-            ui.clientPortal.click_performance_panel()
-                .verify_performance_titles()
-                .verify_performance_card('0', '2')
-                .verify_performance_card('1', '3')
-                .verify_performance_card('2', '4')
-                .verify_performance_card('3', '5')
-        } catch (error) {
-            if (error.code === 'ECONNRESET') {
-                cy.log('Skipping test due to ECONNRESET error')
-                return;
 
-            }
-        }
-
+        ui.clientPortal.click_performance_panel()
+            .verify_performance_titles()
+            .verify_performance_card('0', '2')
+            .verify_performance_card('1', '3')
+            .verify_performance_card('2', '4')
+            .verify_performance_card('3', '5')
 
     })
 
