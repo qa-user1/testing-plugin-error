495e0c0ef0e88af6ea1467ccd22aeac3c67f0352
AmminaSK <senderovic.amina@gmail.com>
2023-03-24 09:06:14 +0100

    all tests (13 and 14 broken)

--- cypress.config.js
+++ cypress.config.js
@@ -38,8 +38,6 @@ module.exports = defineConfig({
     },*/
 
     e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       const getCompareSnapshotsPlugin = require('cypress-visual-regression/dist/plugin');
       getCompareSnapshotsPlugin(on, config);
