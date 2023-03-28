fff7bae33749535da3a7c4c22e649202e634db75
sumejja-tracker <59477886+sumejja-tracker@users.noreply.github.com>
2023-03-28 11:45:34 +0200

    2 tests

--- cypress/specs/6-createCompanyAccount-spec.js
+++ cypress/specs/6-createCompanyAccount-spec.js
@@ -14,10 +14,10 @@ context('Company Account', () => {
 
 
 
-    it('1. Validate login credentials', function () {
+    it.only('1. Validate login credentials', function () {
         ui.login.open_base_url()
             .verify_login_menu(D.user)
-            .enter_credentials_and_click_Sign_In(D.user.username, D.user.password)
+            .enter_credentials_and_click_Sign_In(D.user.username, 'test')
             .redirect_user_to_the_create_a_new_account_page()
         ui.onboarding.verify_account_selection()
     })
