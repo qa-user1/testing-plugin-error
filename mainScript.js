70ee0918c475ec8e98766af5398d5e97d5e42c99
AmminaSK <senderovic.amina@gmail.com>
2023-05-30 23:41:57 +0200

    ...

--- mainScript.js
+++ mainScript.js
@@ -2,11 +2,8 @@ const fs = require('fs');
 var path = require('path');
 const AWS = require('aws-sdk');
 const local = process.argv[2];
+const {  BUCKET_NAME, AWS_ACCESS_ID, AWS_SECRET_KEY } = process.env;
 
-//const {  BUCKET_NAME, AWS_ACCESS_ID, AWS_SECRET_KEY } = process.env;
-const BUCKET_NAME = 'neutron-qa-data',
-    AWS_ACCESS_ID = 'AKIAXFS6WV4DEGL5JFO7',
-    AWS_SECRET_KEY = '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'
 
 function uploadToS3(file, name, type) {
     console.log(process.env.BUCKET_NAME);
